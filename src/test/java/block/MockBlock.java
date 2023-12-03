487b3bf1489390a955f9de17b718a1b5cd857069
Saltator <saltatorv@gmail.com>
2023-12-03 18:11:01 +0100

    tests: create MockBlock (#3)

--- src/test/java/block/MockBlock.java
+++ src/test/java/block/MockBlock.java
@@ -1,2 +1,26 @@
-package block;public class MockBlock {
+package block;
+
+public class MockBlock implements Block{
+
+    private final String color;
+    private final String material;
+
+    private MockBlock(String color, String material) {
+        this.color = color;
+        this.material = material;
+    }
+
+    public static MockBlock createDefault() {
+        return new MockBlock("black", "wood");
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String getMaterial() {
+        return material;
+    }
 }
