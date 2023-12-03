0c4be9aad759a6dd69a6d34ef43c296c6a8ce39e
Saltator <saltatorv@gmail.com>
2023-12-03 18:06:35 +0100

    tests: create test for Wall::count (#3)

--- src/test/java/structure/WallTest.java
+++ src/test/java/structure/WallTest.java
@@ -10,6 +10,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WallTest {
 
+
+
+    @Test
+    public void testWallCount() {
+        //given
+        List<Block> blocks = List.of();
+        var wall = Wall.create(blocks);
+
+        //when
+        var count = wall.count();
+
+        //then
+        assertEquals(0, count);
+
+    }
+
     @Test
     public void testCreateWall() {
         //given
@@ -21,5 +37,4 @@ public class WallTest {
         //then
         assertEquals(0, wall.count());
     }
-
 }
